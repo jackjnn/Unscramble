57d7e466fecef79557be7f84e33d9ab99c6e52e4
jackjnn <jacknyangenj.jn2@gmail.com>
2021-04-06 23:58:18 +0300

    Added currentWord class variable and getNextWord

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -13,6 +13,13 @@ class GameViewModel : ViewModel() {
     private var _currentScrambledWord = "test"
     val currentScrambledWord: String
         get() = _currentScrambledWord
+    private var wordsList: MutableList<String> = mutableListOf()
+    private lateinit var currentWord: String
+    private fun getNextWord() {
+        currentWord = allWordsList.random()
+        val tempWord = currentWord.toCharArray()
+        tempWord.shuffle()
+    }
 
     override fun onCleared() {
         super.onCleared()
